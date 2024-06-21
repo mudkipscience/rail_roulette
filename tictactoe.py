3a0a3320da575ceb3c9786f41dce415cafb431db
Emily <me@emily.im>
2024-06-21 12:56:43 +1000

    board generated instead of static, added comments
    - Board is now generated via a function instead of being a pre-defined variable
    - Added comments so I know wtf I was doing in 2 months time
    - format_board() now returns a string
    - removed herobrine

--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,8 @@
-board = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
-    ]
-    
+board = []
 plr1_char = 'X'
 plr2_char = 'O'
-b_size = len(board)
-b_range = range(b_size)
 
-
-def row_winner(board):
+def row_winner(board): # Checks whether values in a nested list from board are all identical, if they are this is a win!
     for row in board:
         first_run = True
         previous = ''
@@ -31,75 +23,82 @@ def row_winner(board):
                 
     return False
 
-def column_winner(board):
+def column_winner(board): # Takes the board and recreates it ([['X', 'O], ['X', ' ']] becomes [['X', 'X'], ['O'], [' ']]) and then calls row_winner() to reuse some code I prepared earlier :3
     c_x = 0
     c_board = []
     
-    for i in b_range: # Loop as many times as we need to get through all values on the board
+    for i in range(len(board)): # Loop as many times as we need to get through all values on the board
         c_y = 0
         c_row = []
         
-        for i in b_range: # Loop through all nested lists (rows) and get value of first index in each row
+        for i in range(len(board)): # Loop through all nested lists (rows) and get value of first index in each row
             c_row.append(board[c_y][c_x])
             c_y += 1
         
         c_board.append(c_row)
         c_x += 1
     
-    return row_winner(c_board) # Reuse some code I prepared earlier :3
+    return row_winner(c_board) # Is this efficient? I dunno!
 
-def diagonal_winner(board):
+def diagonal_winner(board): # Creates two lists containing the values of each nested list in a diagonal line ([0][0], [1][1], [2][2], etc.) and again passes it to row_winner()
     d_row1 = []
     d_row2 = []
     
-    for i in b_range:
+    for i in range(len(board)):
         d_row1.append(board[i][i]) # Create a new list containing the values of the diagonal board row, top to bottom
     
-    for i in b_range:
+    for i in range(len(board)):
         d_row2.append(board[-i - 1][i]) # Create a new list containing the values of the diagonal board row, top to bottom. THIS TOOK ME LIME HALF ABN HOUR TO FIGURE OUT AND IT WAS SO SIMPLE I FEEL SO STUPDI ARRRGH
         
         
-    return row_winner([d_row1, d_row2]) # Is this efficient? I dunno!
+    return row_winner([d_row1, d_row2])
 
-def winner(board):
+def winner(board): # Don't feel the need to explain this one
     return row_winner(board) or column_winner(board) or diagonal_winner(board)
 
-def endgame_reached(board):
+def endgame_reached(board): # Checks whether the board still contains any blank spaces, for detecting whether the game is unwinnable
     for row in board:
             if ' ' in row:
                 return False
             
     return True
 
-def process_input(valid_inputs):
+def process_input(valid_inputs): # Takes a list of valid choices as arguments and only allows input of characters found in the array. Only works with integers
     choice = int(input('> '))
     if choice in valid_inputs:
         return choice
     else:
         print(str(choice) + ' is not a valid choice.')
         process_input(valid_inputs)
-        
 
-def format_board(): 
+def create_board(size): # Create a board (list of nested lists, all of the same length) taking an int as an argument which is used to generate a larger/smaller board
+    global board # So apparently I can't just change a variable declared outside of a function or it will create a NEW local variable that doesn't affect the global one. Unless I do this!
+    board = [] # Clear the board so we don't get any funny behaviour when this function is repeatedly called
+    for i in range(size):
+        row = []
+        for i in range(size):
+            row.append(' ')
+        board.append(row.copy())
+
+def format_board(): # Draws the board by adding a key (numbers on the edge of the X and Y axis so players know what spaces are which) and then joining all lists together, returning a string
     board_size = len(board)
-    
-    top = '  ' # Empty whitespace to pad key so it doesn't look ugly
+    outp = '  ' # Empty whitespace to pad key so it doesn't look ugly
     
     for p in range(board_size):
-        top += str(p + 1) + ' ' # Generates the key at the top of the board
-    
-    print(top)
-    
+        outp += str(p + 1) + ' ' # Generates the key at the top of the board
+
     for i in range(board_size):
-        print(f"{i + 1} {'|'.join(board[i])}") # converts a single "row" inside the board list into a string so it can be displayed
+        outp += f"\n{i + 1} {'|'.join(board[i])}" # converts a single "row" inside the board list into a string so it can be displayed
         
         if (i + 1) != board_size: # generates the spacing "-+-+-" between rows, skipping the last one
-            print('  ' + '-+' * (board_size - 1) + '-')
+            outp += '\n  ' + '-+' * (board_size - 1) + '-'
+
+    return(outp)
 
 def ops_menu():
     print('-+ Options +-')
     print()
-    print(f'1) Change board size ({b_size}x{b_size})')
+    print(f'1) Change board size ({len(board)}x{len(board)})')
     print(f'2) Change player 1 character ({plr1_char})')
     print(f'3) Change player 2 character ({plr2_char})')
     print()
@@ -113,8 +112,10 @@ def main_menu():
     print()
     inp = process_input([1, 2])
     if inp == 1:
-        format_board()
+        print(format_board())
     else:
         ops_menu()
 
+# -+ Actual game code begins here, the above was all just logic +-
+create_board(3)
 main_menu()
