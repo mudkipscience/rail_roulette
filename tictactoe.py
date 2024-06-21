e919792482b732ff557b93a26a1202c46f6eea8c
Emily <me@emily.im>
2024-06-21 13:22:52 +1000

    shuffled things around for no reason

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,40 @@ board = []
 plr1_char = 'X'
 plr2_char = 'O'
 
+def create_board(size = 3): # Create a board (list of nested lists, all of the same length) taking an int as an argument which is used to generate a larger/smaller board
+    global board # So apparently I can't just change a variable declared outside of a function or it will create a NEW local variable that doesn't affect the global one. Unless I do this!
+    board = [] # Clear the board so we don't get any funny behaviour when this function is repeatedly called
+    for i in range(size):
+        row = []
+        for i in range(size):
+            row.append(' ')
+        board.append(row.copy())
+
+def format_board(): # Draws the board by adding a key (numbers on the edge of the X and Y axis so players know what spaces are which) and then joining all lists together, returning a string
+    board_size = len(board)
+    outp = '  ' # Empty whitespace to pad key so it doesn't look ugly
+    
+    for p in range(board_size):
+        outp += str(p + 1) + ' ' # Generates the key at the top of the board
+
+    for i in range(board_size):
+        outp += f"\n{i + 1} {'|'.join(board[i])}" # converts a single "row" inside the board list into a string so it can be displayed
+        
+        if (i + 1) != board_size: # generates the spacing "-+-+-" between rows, skipping the last one
+            outp += '\n  ' + '-+' * (board_size - 1) + '-'
+
+    return(outp)
+
+def process_input(valid_inputs): # Takes a list of valid choices as arguments and only allows input of characters found in the array. Only works with integers
+    choice = int(input('> '))
+    if choice in valid_inputs or len(valid_inputs) == 0:
+        return choice
+    else:
+        print(str(choice) + ' is not a valid choice.')
+        process_input(valid_inputs)
+
+# -+ Win detection logic +-
+
 def row_winner(board): # Checks whether values in a nested list from board are all identical, if they are this is a win!
     for row in board:
         first_run = True
@@ -63,64 +97,34 @@ def endgame_reached(board): # Checks whether the board still contains any blank 
             
     return True
 
-def process_input(valid_inputs): # Takes a list of valid choices as arguments and only allows input of characters found in the array. Only works with integers
-    choice = int(input('> '))
-    if choice in valid_inputs or len(valid_inputs) == 0:
-        return choice
-    else:
-        print(str(choice) + ' is not a valid choice.')
-        process_input(valid_inputs)
-
-def create_board(size = 3): # Create a board (list of nested lists, all of the same length) taking an int as an argument which is used to generate a larger/smaller board
-    global board # So apparently I can't just change a variable declared outside of a function or it will create a NEW local variable that doesn't affect the global one. Unless I do this!
-    board = [] # Clear the board so we don't get any funny behaviour when this function is repeatedly called
-    for i in range(size):
-        row = []
-        for i in range(size):
-            row.append(' ')
-        board.append(row.copy())
-
-def format_board(): # Draws the board by adding a key (numbers on the edge of the X and Y axis so players know what spaces are which) and then joining all lists together, returning a string
-    board_size = len(board)
-    outp = '  ' # Empty whitespace to pad key so it doesn't look ugly
-    
-    for p in range(board_size):
-        outp += str(p + 1) + ' ' # Generates the key at the top of the board
-
-    for i in range(board_size):
-        outp += f"\n{i + 1} {'|'.join(board[i])}" # converts a single "row" inside the board list into a string so it can be displayed
-        
-        if (i + 1) != board_size: # generates the spacing "-+-+-" between rows, skipping the last one
-            outp += '\n  ' + '-+' * (board_size - 1) + '-'
-
-    return(outp)
+# -+ Menu stuff +-
 
 def ops_menu():
-    print('-+ Options +-')
-    print()
+    print('-+ Options +-\n')
     print(f'1) Change board size ({len(board)}x{len(board)})')
     print(f'2) Change player 1 character ({plr1_char})')
-    print(f'3) Change player 2 character ({plr2_char})')
-    print()
+    print(f'3) Change player 2 character ({plr2_char})\n')
+
     inp = process_input([1, 2, 3])
+
     if inp == 1: # Change board size
-        print('Input a new board size below.')
-        print()
+        print('Input a new board size below.\n')
         inp = process_input([])
         create_board(inp)
-        main_menu()
     elif inp == 2: # Change the character player 1 is represented as
         ...
     else: # Change the character player 2 is represented as
         ...
+    
+    main_menu()
 
 def main_menu():
-    print('-+ Tic Tac Toe +-')
-    print()
+    print('-+ Tic Tac Toe +-\n')
     print('1) Play')
-    print('2) Options')
-    print()
+    print('2) Options\n')
+
     inp = process_input([1, 2])
+    
     if inp == 1:
         print(format_board())
     else:
